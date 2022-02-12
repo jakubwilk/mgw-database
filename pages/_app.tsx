9f70f21e15b8ada8d3a92f370cc0d0a85005cbd9
jakubwilk <jw@jakubwilk.pl>
2022-02-12 18:59:19 +0100

    Create custom Document component and change css to scss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { MantineProvider } from '@mantine/core'
+import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <MantineProvider theme={{}}>
+            <Component {...pageProps} />
+        </MantineProvider>
+    )
 }
 
 export default MyApp
