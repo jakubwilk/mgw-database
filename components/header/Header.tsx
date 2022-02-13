83bd92704ddc82040b2888d864c99060f6cd35c1
jakubwilk <jw@jakubwilk.pl>
2022-02-13 16:09:33 +0100

    Upload logo and update Header and Navbar components

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,19 +1,35 @@
 import { useContext } from 'react'
-import { Burger, Header, MediaQuery, Text } from '@mantine/core'
+import Image from 'next/image'
+import { Burger, Header, MediaQuery, Text, useMantineTheme } from '@mantine/core'
 import { IContext } from '../../utils/interfaces/IContext'
 import { AppContext } from '../../context/AppContext'
+import styles from './Header.module.scss'
 
 export const AppHeader = () => {
     const appContext = useContext<IContext>(AppContext)
     const { isSidebarOpen, setIsSidebarOpen } = appContext
+    const theme = useMantineTheme()
 
     return (
-        <Header height={70} padding={'md'}>
-            <div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
+        <Header className={styles.header} height={120}>
+            <div className={styles.headerContainer}>
                 <MediaQuery largerThan={'sm'} styles={{ display: 'none' }}>
-                    <Burger opened={isSidebarOpen} onClick={() => setIsSidebarOpen(!isSidebarOpen)} size={'sm'} mr={'xl'} />
+                    <Burger
+                        opened={isSidebarOpen}
+                        onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+                        color={theme.colors.gray[1]}
+                        size={'lg'}
+                        className={styles.menuButton}
+                    />
                 </MediaQuery>
-                <Text>{'Application header'}</Text>
+                <div className={styles.logoWrapper}>
+                    <Image
+                        src={'/logo.png'}
+                        alt={'Napis Mage Guild Wars - play by forum Fairy Tail'}
+                        layout={'fill'}
+                        className={styles.logoImage}
+                    />
+                </div>
             </div>
         </Header>
     )
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -2,16 +2,21 @@ import { useContext } from 'react'
 import Link from 'next/link'
 import { IContext } from '../../utils/interfaces/IContext'
 import { AppContext } from '../../context/AppContext'
-import { Navbar, Text } from '@mantine/core'
+import { Navbar, ScrollArea, Text } from '@mantine/core'
+import styles from './Navbar.module.scss'
 
 export const AppNavbar = () => {
     const appContext = useContext<IContext>(AppContext)
     const { isSidebarOpen } = appContext
 
     return (
-        <Navbar padding={'md'} hiddenBreakpoint={'sm'} hidden={!isSidebarOpen} width={{ sm: 300, lg: 400 }}>
-            <Link href={'/'}>{'Strona główna'}</Link>
-            <Link href={'/guides'}>{'Poradniki'}</Link>
+        <Navbar padding={'md'} hiddenBreakpoint={'sm'} hidden={!isSidebarOpen} width={{ sm: 300, lg: 400 }} className={styles.sidebar}>
+            <Navbar.Section>{'Nawigacja'}</Navbar.Section>
+            <Navbar.Section grow component={ScrollArea}>
+                <Link href={'/'}>{'Strona główna'}</Link>
+                <Link href={'/guides'}>{'Poradniki'}</Link>
+            </Navbar.Section>
+            <Navbar.Section>{'Wróć na forum'}</Navbar.Section>
         </Navbar>
     )
 }
